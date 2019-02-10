f64c25a5aa43569744e4c4cf6e36bc36b3689c35
Bruce Allan Makaaru <brucemakallan@MacBook-Pro-29.local>
2019-02-10 18:06:15 +0300

    Use nodemon

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Welcome'));
+app.get('/', (req, res) => res.send('Welcome!'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
