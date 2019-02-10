40f0030a26764026286290fcda0f7bad82f0698e
Bruce Allan Makaaru <brucemakallan@MacBook-Pro-29.local>
2019-02-10 18:14:32 +0300

    Setup environment variables handling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Welcome!'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
